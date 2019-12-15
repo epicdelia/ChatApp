9c1894ec017b2c4047dffb947c367432f9342679
epicdelia <epicdelia@gmail.com>
2019-12-15 01:42:08 -0500

    Added Nav and started main acticity UI

---
+++ app/src/main/java/com/epicdelia/smackchat/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.epicdelia.smackchat.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Please Login"
+    }
+    val text: LiveData<String> = _text
+}
